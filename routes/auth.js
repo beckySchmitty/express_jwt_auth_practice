d12d72e6d171bb485502c0b7ec0ab0c4e9dc7ccc
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-30 09:51:00 -0500

    successfully added login route

--- routes/auth.js
+++ routes/auth.js
@@ -16,25 +16,45 @@ router.get('/', (req, res, next) => {
 router.post('/register', async (req, res, next) => {
 try {
   const {username, password} = req.body;
-
   if (!username || !password) {
     throw new ExpressError("username and password required", 400)
   }
   const hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
   // save to db
-  const result = await db.query(`INSERT INTO users (username, password)
+  const results = await db.query(`INSERT INTO users (username, password)
   VALUES ($1, $2) 
   RETURNING username `, [username, hashedPwd]);
 
-  return res.json(result.rows[0])
+  return res.json(results.rows[0])
 } catch(e) {
   if (e.code === '23505') {
     return next(new ExpressError("username taken, pick another", 400));
   }
   return next(e)
 }
-
 })
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    if (!username || !password) {
+      throw new ExpressError("username and password required", 400)
+    }
+    const results = await db.query(`SELECT username, password 
+    FROM users 
+    WHERE username=$1`, [username]);
+    const user = results.rows[0];
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        return res.json({message: `Welcome back ${user.username}`})
+      }
+    }
+    throw new ExpressError("Invalid username/password", 400)
+  } catch(e) {
+    return next(e)
+  }
+  })
+  
 
 
 
