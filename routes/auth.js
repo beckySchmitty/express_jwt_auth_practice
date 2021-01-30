b0dcc81adc31d7feabc912320f3387e66158bc86
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-30 09:39:05 -0500

    successfully added register route

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,29 @@ router.get('/', (req, res, next) => {
 })
 
 
+router.post('/register', async (req, res, next) => {
+try {
+  const {username, password} = req.body;
+
+  if (!username || !password) {
+    throw new ExpressError("username and password required", 400)
+  }
+  const hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+  // save to db
+  const result = await db.query(`INSERT INTO users (username, password)
+  VALUES ($1, $2) 
+  RETURNING username `, [username, hashedPwd]);
+
+  return res.json(result.rows[0])
+} catch(e) {
+  if (e.code === '23505') {
+    return next(new ExpressError("username taken, pick another", 400));
+  }
+  return next(e)
+}
+
+})
+
 
 
 module.exports = router;
